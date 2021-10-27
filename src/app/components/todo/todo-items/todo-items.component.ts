debe8d704888140e1aefde498598af8a52e95e0c
Jovan <radosavljevic.jovan@gmail.com>
2021-10-27 11:43:39 +0200

    Paginate commit

--- src/app/components/todo/todo-items/todo-items.component.ts
+++ src/app/components/todo/todo-items/todo-items.component.ts
@@ -4,17 +4,13 @@ import { ITodo } from 'src/app/services/todo.service';
 @Component({
   selector: 'app-todo-items',
   templateUrl: './todo-items.component.html',
-  styleUrls: ['./todo-items.component.scss']
+  styleUrls: ['./todo-items.component.scss'],
 })
 export class TodoItemsComponent implements OnInit {
-
-
-  @Input() todoItems : ITodo[] = [];
-  @Input() fiveTodos : ITodo[] = [];
+  @Input() todoItems: ITodo[] = [];
+  @Input() fiveTodos: ITodo[] = [];
   public currentLocation = window.location.pathname;
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  page: number = 1;
+  ngOnInit(): void {}
 }
